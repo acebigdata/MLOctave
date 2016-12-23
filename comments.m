82303df972fe75c70542965c59e13b6db1175d71
Isaac Zhou <bigdataraw@gmail.com>
2016-12-23 02:28:18 -0500

    Add Ex files from Lynda

---
+++ comments.m
@@ -0,0 +1,18 @@
+
+
+sales = 800
+
+if (sales >= 750);
+
+	printf("Bonus target reached. \n");
+
+elseif (sales >=500);
+
+	printf("Sales target reached.\n");
+
+else
+
+	printf("Sales target not reached.\n");
+
+endif
+
---
+++ debugging.m
@@ -0,0 +1,18 @@
+#Testing if statements
+
+sales = 800;
+
+if (sales >= 500);
+
+	printf("Bonus target reached. \n");
+
+elseif (sales >=750);
+
+	printf("Sales target reached.\n");
+
+else
+
+	printf("Sales target not reached.\n");
+
+endif
+
